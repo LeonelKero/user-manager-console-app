64af862262837ecce26ed7a8a2dace8fe6d392dd
leonel ka <waboleonel@gmail.com>
2025-01-11 18:09:49 +0100

    Database connection, and insertion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import com.wbt.db.configuration.DatabaseConfig;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException {
+        final var dbManager = DriverManager.getConnection(DatabaseConfig.DATABASE_URL); // Can also pass username and password alongside
+        final var statement = dbManager.createStatement();
+        statement.execute(DatabaseConfig.createUserTable);
+        // insert new user
+        final var pStatement = dbManager.prepareStatement(DatabaseConfig.insertNewUser);
+        pStatement.setString(1, "leonel ka");
+        pStatement.setString(2, "wabo");
+        pStatement.setString(3, "waboleonel@gmail.com");
+        pStatement.setString(4, "(+237)681-592-765");
+        int i = pStatement.executeUpdate();
+        System.out.println("\t\t -------> " + i);
     }
 }
